aa2fdadf6c81cf035b276803e4302b505175e888
Felipe Gonzalez <felipexgonzalez@gmail.com>
2012-04-02 15:36:30 -0500

    prefiles_boot calcula bootstrap para cada corte, y devuelve la Base

--- R/perfiles_boot.R
+++ R/perfiles_boot.R
@@ -1,10 +1,11 @@
-perfiles_boot <- function(datos, header, var, n.boot = 1, marcar.tol = 20){
+perfiles_boot <- function(datos, header, var, n.boot = 100, marcar.tol = 20){
     # Calcular primero puntuales
     dat <- datos[, c(header, var)]
     tablas <- function(datos, header, var){
         tab.raw <- ddply(datos, header, function(df){
             salida <- data.frame(table(df[, var]))
             names(salida) <- c( var, 'Freq')
+            salida$Base <- sum(salida$Freq)
             salida
         })
         tab.prop <- ddply(tab.raw, header, transform, Prop = 100*Freq/sum(Freq))
@@ -12,14 +13,20 @@ perfiles_boot <- function(datos, header, var, n.boot = 1, marcar.tol = 20){
         tab.momios
     }
 
-    muestra.boot <- function(datos){
-         muestra.reng <- sample(1:nrow(datos), nrow(datos), replace = TRUE)
-         datos[muestra.reng, ]
+    muestra.boot <- function(datos, header){
+        datos.boot <- ddply(datos, header, function(df){
+            muestra.reng <- sample(1:nrow(df), nrow(df), replace = TRUE)
+            df[muestra.reng, ]   
+        })
+        datos.boot
     }
     reps <- rdply(n.boot, function(){
-        tablas(muestra.boot(dat), header, var)
+        tablas(muestra.boot(dat, header), header, var)
     })
     out <- ddply(reps, c(header,var), summarise, 
+        Base = mean(Base),
+        Freq = mean(Freq),
+        Prop = mean(Prop),
         Perfil = mean(Perfil),
         Perfil.sd = sd(Perfil),
         Perfil.10 = quantile(Perfil, 0.10),
